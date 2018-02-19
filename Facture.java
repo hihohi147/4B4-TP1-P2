b80eb8bce9fe275ca69d5676ac402b6c93588cb5
William Durocher <durocherwilliam@gmail.com>
2018-02-19 14:12:06 -0500

    Premier commit. creation des 3 classes necessaires au programme.

---
+++ Facture.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Facture {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			
+		
+	}
+}
